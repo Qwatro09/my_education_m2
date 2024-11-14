2e40298858484b68d32ef3fe34e929845815bbfa
AndreSap <andrepq71q@gmail.com>
2024-11-14 17:57:37 +0300

    Add files via upload

---
+++ modul_2_3.py
@@ -0,0 +1,14 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5, 10]
+
+list_ind = 0
+
+while list_ind <= len(my_list):
+    if my_list[list_ind] == 0:
+        list_ind += 1
+        continue
+    if my_list[list_ind] > 0:
+        print(my_list[list_ind])
+        list_ind += 1
+        continue
+    else:
+        break
