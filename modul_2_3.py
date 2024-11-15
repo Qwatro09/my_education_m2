2f6a0406e07159a2506201db02054631617cdbd1
Андрей <andrepq71q@gmail.com>
2024-11-15 20:37:45 +0300

    homework_corrected

--- modul_2_3.py
+++ modul_2_3.py
@@ -1,14 +1,16 @@
-my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+my_list = [42, 69, 322, 13, 0, 99, 5, 9, 8, 7, 6, 5]
 
 list_ind = 0
 
 while list_ind <= len(my_list):
-    if my_list[list_ind] == 0:
-        list_ind += 1
-        continue
-    if my_list[list_ind] > 0:
-        print(my_list[list_ind])
-        list_ind += 1
-        continue
-    else:
+    list_ind += 1
+    if list_ind >= len(my_list):
         break
+    else:
+        if my_list[list_ind] == 0:
+            continue
+        if my_list[list_ind] > 0:
+            print(my_list[list_ind])
+            continue
+        if my_list[list_ind] < 0:
+             break
