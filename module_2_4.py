d6f4e514a8a136f3bb672ad155664a330fbfc9d0
Андрей <andrepq71q@gmail.com>
2024-11-19 11:56:06 +0300

    homework_2_4_correct

--- module_2_4.py
+++ module_2_4.py
@@ -1,25 +1,33 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+import random
+
+numbers = [9, 10, 11, 12, 13, 14, 15, 1, 2, 3, 4, 5, 6, 7, 8]
+
+random.shuffle(numbers)
 
 primes = []
 not_primes = []
 is_prime = True
 
-for i_num in numbers[1:]:
-    for j_num in numbers[1:]:
-       if i_num % j_num == 0 and i_num != j_num:
-          is_prime = False
-          if is_prime is False:
-              not_primes.append(i_num)
-              break
+for i_num in numbers:
+    if i_num == 1:
+        continue
+    else:
+        count = 0
+        for j_num in range(2, int(i_num // 2 + 1)):
+            if (i_num % j_num == 0):
+                count += 1
 
-       if i_num % j_num == 0 and i_num == j_num:
-           is_prime = True
-           if is_prime is True:
-               primes.append(i_num)
-               break
+        if (count <= 0):
+            is_prime = True
+        else:
+            is_prime = False
+
+        if is_prime is True:
+            primes.append(i_num)
+        else:
+            not_primes.append(i_num)
+
 
 print(f'Список: {numbers}')
 print(f'Primes: {primes}')
 print(f'Not Primes: {not_primes}')
-
-
