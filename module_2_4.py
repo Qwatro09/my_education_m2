3c3b5c17bfd8e5c4a2f9b7396f670d24115863a5
Андрей <andrepq71q@gmail.com>
2024-11-20 16:36:14 +0300

    homework_2_4

--- module_2_4.py
+++ module_2_4.py
@@ -1,6 +1,6 @@
 import random
 
-numbers =[]
+numbers = []
 # numbers = [9, 10, 11, 12, 13, 14, 15, 1, 2, 3, 4, 5, 6, 7, 8, 99, 101]
 for r_num in range(10):
     numbers.append(random.randint(1, 100))
@@ -8,27 +8,16 @@ for r_num in range(10):
 
 primes = []
 not_primes = []
-is_prime = True
 
 for i_num in numbers:
-    if i_num == 1:
-        continue
-    else:
-        count = 0
-        for j_num in range(2, int(i_num // 2 + 1)):
-            if (i_num % j_num == 0):
-                count += 1
-                break
-        if (count == 0):
-            is_prime = True
-        else:
+    is_prime = True
+    for j_num in range(2, i_num // 2 + 1):
+        if (i_num % j_num == 0):
             is_prime = False
-
-        if is_prime is True:
-            primes.append(i_num)
-        else:
             not_primes.append(i_num)
-
+            break
+    if is_prime == True and i_num != 1:
+        primes.append(i_num)
 
 print(f'Список: {numbers}')
 print(f'Primes: {primes}')
